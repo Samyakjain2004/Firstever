ac2059f53f67726cc1a68c97bf8a46641bf28c2f
jains <jains@Samy.bbrouter>
2022-12-27 18:31:17 +0530

    Array practice

--- src/main/java/revise/ThirtyThree33.java
+++ src/main/java/revise/ThirtyThree33.java
@@ -3,18 +3,23 @@ package revise;
 public class ThirtyThree33 {
 //Take a number and display it one by one
 	public static void main(String[] args) {
-		double x = 9834;
-		double y=0;
+		int x = 9834;
+		double y = 0;
 		double[] arr = new double[4];
+		double[] arr1 = new double[4];
 		for (int i = 0; i < arr.length; i++) {
-			
-			arr[i] = (x %(Math.pow(10, i+1)))-y;
-			arr[i]=y;
+
+			arr[i] = x % Math.pow(10, i + 1) - y;
+			y = y + arr[i];
 		}
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-			
+			arr1[i] = arr[i] / (10 * Math.pow(10, i));
+			y = arr1[i];
 		}
-		
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr1[i] * 10 + " ");
+
+		}
+
 	}
 }
